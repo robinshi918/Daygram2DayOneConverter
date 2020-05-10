47b72bada8a9dac5f54f89442ccd58653614d70d
Robin Shi <robin.shi@eightdevelopment.com>
2020-05-11 08:51:25 +1200

    regex to find the time tag

--- convert.py
+++ convert.py
@@ -2,17 +2,11 @@
 import re
 
 # December 15 Tuesday 2015 /
-month = ['January', 'February', 'March', 'April',
-'May', 'June', 'July' ,'August', 'September', 'October', 'November' ,'December']
-week_day = ['Monday', 'Tuesday', 'Wednesday', "Thursday", "Friday", 'Saturday', "Sunday"]
-print(month)
-print(week_day)
 
-
-s1 = 'December 15 Tuesday 2015 /'
-pattern = '[January|February|December]'
+s1 = 'December 15 Tuesday 2015 /   December 17 Monday 2015 /  January 15 Wednesday 2016 /'
+pattern = r"(January|February|December) [0-3][0-9] (Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday) 20[0-9][0-9] /"
 for match in re.finditer(pattern, s1):
     s = match.start()
     e = match.end()
-    print 'String match "%s" at %d:%d' % (s1[s:e], s, e)
+    print '"%s" at %d:%d' % (s1[s:e], s, e)
 
