a1cc4351c19bfa550376ee99312ed5dee741a2a7
Robin Shi <robin.shi@eightdevelopment.com>
2020-05-11 21:18:50 +1200

    add sample code to create json data

--- convert.py
+++ convert.py
@@ -1,4 +1,17 @@
 import re
+import json
+
+def createJson():
+    jjj = {
+        "key1": {
+            "key2": [1,2,3],
+            "key3": [4,5,6]
+        }
+    }
+    print(json.dumps(jjj, indent=4))
+
+
+createJson()
 
 # December 15 Tuesday 2015 /
 
@@ -8,6 +21,9 @@ with open('data2.txt', 'r') as myfile:
 pattern = r"(January|February|March|April|May|June|July|August|September|October|November|December|) [0-3][0-9] (Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday) 20[0-9][0-9] /"
 tag_start = 0
 tag_end = 0
+body_start = 0
+body = ""
+
 for match in re.finditer(pattern, data):
     tag_start = match.start()
     tag_end = match.end()
@@ -28,3 +44,6 @@ if tag_start != 0:
     print("last match content")
     print("tag:" + tag)
     print("body:\n" + data[tag_end+1:])
+
+
+
